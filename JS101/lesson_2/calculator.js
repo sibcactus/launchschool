495bd6e62a9a738bd4b67b8d47acc42ead902298
Николай Макаров <nmakarov@profi.ru>
2021-11-05 10:17:46 +0300

    debug

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -5,34 +5,57 @@
 //print the result to the terminal
 const readline = require('readline-sync');
 
-console.log("Welcome to Calculator!");
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log('What\'s the first number?');
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt("Welcome to Calculator!");
+
+prompt('What\'s the first number?');
 let number1 = readline.question();
 
-console.log('What\'s the second number?');
+while (invalidNumber(number1)) {
+  prompt("Doesn't look like a valid number");
+  number1 = readline.question();
+}
+
+prompt('What\'s the second number?');
 let number2 = readline.question();
 
-console.log('What operation would you like to perform:\n1) Add 2) Subtract 3) Multiply 4) Divide');
+while (invalidNumber(number2)) {
+  prompt("Doesn't look like a valid number");
+  number2 = readline.question();
+}
+
+prompt('What operation would you like to perform:\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
 
 let output;
 switch (operation) {
   case '1':
-    output = number1 + number2;
+    output = Number(number1) + Number(number2);
     break;
   case '2':
-    output = number1 - number2;
+    output = Number(number1) - Number(number2);
     break;
   case '3':
-    output = number1 * number2;
+    output = Number(number1) * Number(number2);
     break;
   case '4':
-    output = number1 / number2;
+    output = Number(number1) / Number(number2);
     break;
   default:
     output = 'Wrong operation';
 }
 
-console.log(`The result is ${output}`);
+prompt(`The result is ${output}`);
 
---
+++ JS101/lesson_2/debug2.js
@@ -0,0 +1,8 @@
+// debug2.js
+
+let counter = 1;
+
+while (counter <= 5) {
+  console.log(counter);
+  counter += 1;
+}
